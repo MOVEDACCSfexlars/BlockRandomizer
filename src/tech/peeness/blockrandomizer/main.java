988ebe6e2a71c8c48b34f65f819b628819d39e48
fexlar <63706991+fexlars@users.noreply.github.com>
2021-12-14 12:02:05 -0600

    Added the entire project lol

---
+++ src/tech/peeness/blockrandomizer/main.java
@@ -0,0 +1,45 @@
+package tech.peeness.blockrandomizer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Random;
+
+public class main extends JavaPlugin {
+
+    public FileConfiguration config = this.getConfig();
+
+    @Override
+    public void onEnable() {
+
+        // Set up a config for the server if it doesn't have one.
+        config.addDefault("WaitTime", 600);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                    // Basically grabs a random Item from the material list and gives it to the player
+                    Random r = new Random();
+
+                    Material material = null;
+
+                    material = material.values()[r.nextInt(material.values().length)];
+
+                    player.getInventory().addItem(new ItemStack(material, 1));
+            }
+        }, 20, config.getInt("WaitTime"));
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
